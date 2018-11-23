6b67e8b37a5db166dc54ea9182d24c00dae3ea6c
Novák Nóra <noraanovakk@gmail.com>
2018-11-23 20:25:11 +0100

    comment in js and try another version

--- js/noricalculator.js
+++ js/noricalculator.js
@@ -1,10 +1,18 @@
-var noriNum1 = parseFloat(document.querySelectorAll('.noriNumberInput').value);
-var noriNum2 = parseFloat(document.querySelectorAll('.noriNumberInput').value);
+//var noriNum1 = parseFloat(document.querySelectorAll('.noriNumberInput').value, 10);
+//var noriNum2 = parseFloat(document.querySelectorAll('.noriNumberInput').value, 10);
 
-function noriNumber() {
-  document.querySelector('#noriFirstNumberInput').value = noriNum1;
-  document.querySelector('#noriSecondNumberInput').value = noriNum2;
-}
+//function noriNumber() {
+//  document.querySelector('#noriFirstNumberInput').value = noriNum1;
+//  document.querySelector('#noriSecondNumberInput').value = noriNum2;
+//}
+
+var noriNum1 = function noriNumber() {
+  document.querySelector('#noriFirstNumberInput').value = parseFloat(document.querySelectorAll('.noriNumberInput').value, 10);
+};
+
+var noriNum2 = function noriNumber() {
+  document.querySelector('#noriSecondNumberInput').value = parseFloat(document.querySelectorAll('.noriNumberInput').value, 10);
+};
 
 function noriCalculate() {
   var noriOperation = document.querySelector('#noriOperationSelect').value;
@@ -19,5 +27,11 @@ function noriCalculate() {
   }
   if (noriOperation === 'divide') {
     document.querySelector('#noriResultInput').value = noriNum1 / noriNum2;
+  }
+  if (noriOperation === 'power') {
+    document.querySelector('#noriResultInput').value = noriNum1 ** noriNum2;
+  }
+  if (noriOperation === 'remdivide') {
+    document.querySelector('#noriResultInput').value = noriNum1 % noriNum2;
   }
 }
