020e574be319dbba3a3a10c523faf751f79e054b
noraanovakk <noraanovakk@gmail.com>
2018-11-23 14:35:27 +0100

    add noricalculator.js base

--- js/noricalculator.js
+++ js/noricalculator.js
@@ -1,2 +1,23 @@
-function noricalculator() {
+var noriNum1 = parseFloat(document.querySelectorAll('.noriNumberInput').value);
+var noriNum2 = parseFloat(document.querySelectorAll('.noriNumberInput').value);
+var noriOperation = document.querySelector('#noriOperationSelect').value;
+
+function noriNumber() {
+  document.querySelector('#noriFirstNumberInput').value = noriNum1;
+  document.querySelector('#noriSecondNumberInput').value = noriNum2;
+}
+
+function noriCalculate() {
+  if (noriOperation === 'plus') {
+    document.querySelector('#noriResultInput').value = noriNum1 + noriNum2;
+  }
+  if (noriOperation === 'minus') {
+    document.querySelector('#noriResultInput').value = noriNum1 - noriNum2;
+  }
+  if (noriOperation === 'multiply') {
+    document.querySelector('#noriResultInput').value = noriNum1 * noriNum2;
+  }
+  if (noriOperation === 'divide') {
+    document.querySelector('#noriResultInput').value = noriNum1 / noriNum2;
+  }
 }
