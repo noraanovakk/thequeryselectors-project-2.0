f77eb05f3d10752b49ac311df033b872afc4d25a
Andrea Rékasi <rekasi.anci@gmail.com>
2018-11-24 16:19:57 +0100

    Adding aboutus.html, img folder, calculatot html,js,css

---
+++ anciCalculator.js
@@ -0,0 +1,38 @@
+function insert(num) {
+  var anciOpEmp = document.querySelector('#anciOperators').value === 'empty';
+  if (anciOpEmp === true) {
+    document.calculator.anciNumber1.value = document.calculator.anciNumber1.value + num;
+  } else {
+    document.calculator.anciNumber2.value = document.calculator.anciNumber2.value + num;
+  }
+}
+
+
+function myFunctionEq() {
+  var numberInput1 = parseFloat(
+    document.querySelector('#anciNumber1').value, 10);
+
+  var numberInput2 = parseFloat(
+    document.querySelector('#anciNumber2').value, 10);
+
+  var anciOper = document.querySelector('#anciOperators').value;
+
+  if (anciOper === 'anciPlus') {
+    document.querySelector('#anciResult').value = numberInput1 + numberInput2;
+  }
+  if (anciOper === 'anciMinus') {
+    document.querySelector('#anciResult').value = numberInput1 - numberInput2;
+  }
+  if (anciOper === 'anciMultiply') {
+    document.querySelector('#anciResult').value = numberInput1 * numberInput2;
+  }
+  if (anciOper === 'anciDivision') {
+    document.querySelector('#anciResult').value = numberInput1 / numberInput2;
+  }
+  if (anciOper === 'anciDivisionWithRemainder') {
+    document.querySelector('#anciResult').value = numberInput1 % numberInput2;
+  }
+  if (anciOper === 'anciInvolving') {
+    document.querySelector('#anciResult').value = numberInput1 ** numberInput2;
+  }
+}
