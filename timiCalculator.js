a0baba9e5265e7a9c3c9b44f993ebe8551dcf724
Varga Tímea <myzare@hotmail.com>
2018-11-25 16:06:23 +0100

    "calculator -> tcalculator"

--- timiCalculator.js
+++ timiCalculator.js
@@ -1,38 +1,37 @@
 function tinsert(num) {
-  var tUres = document.querySelector('#tOperatorz').value === 'empty';
+  var tUres = document.querySelector("#tOperatorz").value === "empty";
   if (tUres === true) {
-    document.calculator.tNumOne.value = document.calculator.tNumOne.value + num;
+    document.tcalculator.tNumOne.value =
+      document.tcalculator.tNumOne.value + num;
   } else {
-    document.calculator.tNumTwo.value = document.calculator.tNumTwo.value + num;
+    document.tcalculator.tNumTwo.value =
+      document.tcalculator.tNumTwo.value + num;
   }
 }
 
-
 function tSzamol() {
-  var elsoSzam = parseFloat(
-    document.querySelector('#tNumOne').value, 10);
+  var elsoSzam = parseFloat(document.querySelector("#tNumOne").value, 10);
 
-  var masodikSzam = parseFloat(
-    document.querySelector('#tNumTwo').value, 10);
+  var masodikSzam = parseFloat(document.querySelector("#tNumTwo").value, 10);
 
-  var tOP = document.querySelector('#tOperatorz').value;
+  var tOP = document.querySelector("#tOperatorz").value;
 
-  if (tOP === 'tPlus') {
-    document.querySelector('#tResult').value = elsoSzam + masodikSzam;
+  if (tOP === "tPlus") {
+    document.querySelector("#tResult").value = elsoSzam + masodikSzam;
   }
-  if (tOP === 'tMinus') {
-    document.querySelector('#tResult').value = elsoSzam - masodikSzam;
+  if (tOP === "tMinus") {
+    document.querySelector("#tResult").value = elsoSzam - masodikSzam;
   }
-  if (tOP === 'tMultiply') {
-    document.querySelector('#tResult').value = elsoSzam * masodikSzam;
+  if (tOP === "tMultiply") {
+    document.querySelector("#tResult").value = elsoSzam * masodikSzam;
   }
-  if (tOP === 'tDiv') {
-    document.querySelector('#tResult').value = elsoSzam / masodikSzam;
+  if (tOP === "tDiv") {
+    document.querySelector("#tResult").value = elsoSzam / masodikSzam;
   }
-  if (tOP === 'tDWithR') {
-    document.querySelector('#tResult').value = elsoSzam % masodikSzam;
+  if (tOP === "tDWithR") {
+    document.querySelector("#tResult").value = elsoSzam % masodikSzam;
   }
-  if (tOP === 'tInvo') {
-    document.querySelector('#tResult').value = elsoSzam ** masodikSzam;
+  if (tOP === "tInvo") {
+    document.querySelector("#tResult").value = elsoSzam ** masodikSzam;
   }
 }
